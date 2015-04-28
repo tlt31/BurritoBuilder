f8b76ef1fdde0d36ddaf955f1e0032e598523ae2
tim <tlt31@uakron.edu>
2015-04-28 19:00:44 -0400

    Initial commit

---
+++ src/BurritoBuilder.java
@@ -0,0 +1,17 @@
+/**
+ * Created by tim on 4/28/15.
+ */
+public class BurritoBuilder {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Katie's Burrito Barn!");
+        System.out.println(" ");
+
+
+    }
+
+    static class Burrito{
+
+    }
+}
